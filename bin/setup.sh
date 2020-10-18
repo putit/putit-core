2181111a6d31ab7deb45658bc76c02dccfbfe0f6
depnode_2 <skowpio@gmail.com>
2020-10-18 15:53:36 +0200

    typo

--- bin/setup.sh
+++ bin/setup.sh
@@ -249,7 +249,7 @@ install_bundler_gems() {
   # case for new sqlite3 which is deliverd by putit team. Should apply for Centos 7 only. 
   if ! [ -z ${SQLITE3_PATH+x} ]; then
     ${BUNDLE} config --local build.sqlite3 \ 
-      --with-opt-include=${SQLITE3_PATH}/\
+      --with-opt-include=${SQLITE3_PATH}/ \
       --with-opt-lib=${SQLITE3_PATH}/lib \
       --with-cflags='-O3 -DSQLITE_ENABLE_ICU' \
       --with-cppflags='icu-config --cppflags' \
